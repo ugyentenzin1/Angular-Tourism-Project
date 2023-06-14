667d052beee24d29b129b7eed354a729f1554a2e
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2023-06-14 18:16:10 +0600

    added subscriptio

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,28 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Package} from "../Interfaces/packages";
 import {PackagesService} from "../Services/packages.service";
 import {Router} from "@angular/router";
 import {AnimationStyleMetadata, style} from "@angular/animations";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-packages',
   templateUrl: './packages.component.html',
   styleUrls: ['./packages.component.scss']
 })
-export class PackagesComponent implements OnInit {
+export class PackagesComponent implements OnInit, OnDestroy {
 
   packages!: Package[];
   test!: AnimationStyleMetadata;
-
+  subscription!: Subscription;
   packageShow: boolean = false;
 
   constructor(private packageService: PackagesService,
               private router: Router) { }
 
   ngOnInit(): void {
-    this.packageService.getPackages().subscribe(
+  this.subscription =  this.packageService.getPackages().subscribe(
       (val) => this.packages = val
     );
+  }
+
+  ngOnDestroy():void {
+    this.subscription && this.subscription.unsubscribe();
   }
 
   navigateToEachComponent(id:number):void {
